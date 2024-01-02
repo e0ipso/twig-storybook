3659895de2309f9b95a35aebc1a423c3e524a1f3
Mateu Aguiló Bosch (e0ipso) <mateu@mateuaguilo.com>
2024-01-02 07:54:16 +0100

    ci: fiddle with semantic release configuration

--- release.config.js
+++ release.config.js
@@ -1,17 +1,14 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
-module.exports = {
-  branches: [{ name: 'main' }, { name: 'next' }],
+const config = {
+  branches: [{name: 'main'}, {name: 'next'}],
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        'preset': 'conventionalcommits'
-      }
-    ],
+    ['@semantic-release/commit-analyzer', {'preset': 'conventionalcommits'}],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     '@semantic-release/git',
   ]
 };
+
+export default config;
